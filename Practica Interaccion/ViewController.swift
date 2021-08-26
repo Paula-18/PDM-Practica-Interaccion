229384659a721bf70961b162030e61bd914b1b90
analourdescr <ana_lourdes98@hotmail.com>
2021-08-26 16:10:15 -0700

    Initial Commit

---
+++ Practica Interaccion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Practica Interaccion
+//
+//  Created by Alumno on 8/26/21.
+//  Copyright © 2021 Alumno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
